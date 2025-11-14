9861679f2167c14eb02af42a3c86de4f892ccaf6
JuanSebastianSU <jsebastiansantacruzurgilez2005@gmail.com>
2025-11-14 11:40:38 -0500

    Actualizacion del Frontend

--- src/main/java/com/cursosonline/cursosonlinejs/Seguridad/CorsConfig.java
+++ src/main/java/com/cursosonline/cursosonlinejs/Seguridad/CorsConfig.java
@@ -1,27 +1,3 @@
-// package com.cursosonline.cursosonlinejs.Seguridad;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// public class CorsConfig {
-//   @Bean
-//   public WebMvcConfigurer corsConfigurer() {
-//     return new WebMvcConfigurer() {
-//       @Override
-//       public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/api/v1/**")
-//                 .allowedOriginPatterns("http://localhost:9090", "http://localhost:3000")
-//                 .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
-//                 .allowedHeaders("Authorization","Content-Type")
-//                 .exposedHeaders("Authorization")
-//                 .allowCredentials(true);
-//       }
-//     };
-//   }
-// }
 // CorsConfig.java
 package com.cursosonline.cursosonlinejs.Seguridad;
 
@@ -39,8 +15,9 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
-                    "http://localhost:3000",   // UI React
-                    "http://localhost:9090"    // si usas otro host local
+                    "http://localhost:3000",                         // React local
+                    "http://localhost:9090",                         // otro host local
+                    "https://cursosonline-juan-santacruz.vercel.app" // FRONT en Vercel
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
